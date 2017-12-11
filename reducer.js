07c4e656d88133d74fa090068ce9df217769e342
Ariel Herman <aeh.herman@gmail.com>
2017-12-11 11:27:42 -0500

    done, add decrease count

--- reducer.js
+++ reducer.js
@@ -1,9 +1,35 @@
 let state;
-function changeState(state = {count: 0}, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+function changeState(state = { count: 0 }, action) {
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      return { count: state.count + 1 };
+    case "DECREASE_COUNT":
+      return { count: state.count - 1 };
+
+    default:
+      return state;
   }
+}
+
+let increase = document.getElementById("increase");
+let decrease = document.getElementById("decrease");
+
+increase.addEventListener("click", function() {
+  dispatch({ type: "INCREASE_COUNT" });
+});
+
+decrease.addEventListener("click", function() {
+  dispatch({ type: "DECREASE_COUNT" });
+});
+
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
+function render() {
+  let container = document.getElementById("container");
+  container.textContent = state.count;
+}
+
+dispatch({ type: "@@INIT" });
